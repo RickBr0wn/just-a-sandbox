3e17bc315b244d16ca5548f1600cc6e51feb299c
RiCK BROWN <ricky.brown.00@gmail.com>
2019-01-21 12:57:05 +0000

    Returned master branch to default, cleaned create-react-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import React from 'react'
-import Toggle from './RenderPropsStuff/ToggleRPC'
-import Counter from './ReduxStuff/Counter'
 
 const App = () => {
   return (
     <div>
-      <h1>RENDER PROPS</h1>
-      <Toggle>
-        {({ on, toggle, test }) => (
-          <>
-            {on && <h1>{test}</h1>}
-            <button onClick={toggle}>Show / Hide</button>
-          </>
-        )}
-      </Toggle>
-      <br />
-      <br />
-      <br />
-      <Counter />
+      <h1>Just a sandbox!</h1>
     </div>
   )
 }
